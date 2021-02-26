228501874a98f240592b334c5d5715d92bede6c6
Halley <halley.is.me@gmail.com>
2021-02-25 20:35:26 -0600

    spa day validation studio - almost complete, errors not showing

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,37 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message= "This is a required field.")
+    @Size(min=5, max=15, message="Must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message= "Not a valid Email.")
     private String email;
+
+    @Size(min=6)
+    @NotBlank(message="This is a required field.")
+    @NotNull(message="Please enter a password.")
     private String password;
 
-    public User() {
+    @Size(min=6)
+    @NotBlank(message="This is a required field.")
+    @NotNull(message="Please verify password.")
+    private String verify;
 
-    }
 
-    public User(String username, String email, String password) {
-        this();
+    public User() {}
+
+    public User(String username, String email, String password, String verify) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -38,5 +56,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 }
